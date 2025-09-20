e78d629b998911b6573eec7cef8f9a891695334e
TusharMinche <tusharminche@gmail.com>
2025-09-20 15:46:29 +0530

    Code update

--- src/components/App.js
+++ src/components/App.js
@@ -1,82 +1,13 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import "./../styles/App.css";
 
-import PostHeader from "./PostHeader";
-import PostForm from "./PostForm";
-import Posts from "./Posts";
-import Users from "./Users";
-import Notifications from "./Notifications";
-import User from "./User";
-import Post from "./Post";
+import React from "react";
+import './../styles/App.css';
 
-
-const preDefinedPosts = [
-  {
-    id: 0,
-    title: "Hello World",
-    author: "Uriah Pagac",
-    content: "This is my first post!",
-    reactions: { thumbsUp: 2, hooray: 1, heart: 3, rocket: 0, eyes: 0 },
-  },
-  {
-    id: 1,
-    title: "React Learning",
-    author: "Lauren Bednar",
-    content: "Just started learning React, it’s awesome 🚀",
-    reactions: { thumbsUp: 1, hooray: 0, heart: 2, rocket: 2, eyes: 0 },
-  },
-  {
-    id: 2,
-    title: "Weekend Vibes",
-    author: "Magnus Gislason",
-    content: "Enjoying the weekend with friends 🌸",
-    reactions: { thumbsUp: 0, hooray: 1, heart: 1, rocket: 0, eyes: 0 },
-  },
-];
-
-
-
-function App() {
-  const [posts, setPosts] = useState(preDefinedPosts);
-  const [users] = useState(["Uriah Pagac", "Lauran Bednar", "Magnus Gislason"]);
-  const [notifications, setNotifications] = useState([]);
-
-  // 👇 wrapper to add new posts
-  const onAddPost = (newPost) => {
-    setPosts([...posts, newPost]);
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <h1>GenZ</h1> 
-      <PostHeader />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <PostForm users={users} onAddPost={onAddPost} />
-              <Posts posts={posts} />
-            </>
-          }
-        />
-        <Route path="/users" element={<Users users={users} />} />
-        <Route path="/users/:id" element={<User users={users} posts={posts} />} />
-        <Route
-          path="/notifications"
-          element={
-            <Notifications
-              notifications={notifications}
-              setNotifications={setNotifications}
-            />
-          }
-        />
-        <Route path="/posts/:id" element={<Post posts={posts} setPosts={setPosts} />} />
-      </Routes>
+    <div>
+        {/* Do not remove the main div */}
     </div>
-  );
+  )
 }
 
-
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import { BrowserRouter } from "react-router-dom";
-import "./styles/App.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
+
+import { fetchUsers } from './features/users/usersSlice'
+
+import './api/server'
+
+store.dispatch(fetchUsers())
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
